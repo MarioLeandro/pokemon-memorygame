34de13273230f0a04f1c531f5ea1437cd42b8f0b
unknown <marioleandro34@gmail.com>
2022-02-26 07:41:37 -0300

    shuffle cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+  const initialCards =  [
+    { "src": "/images/bulbasaur.png" },
+    { "src": "/images/butterfree.png" },
+    { "src": "/images/charmander.png" },
+    { "src": "/images/pidgeotto.png" },
+    { "src": "/images/pikachu.png" },
+    { "src": "/images/squirtle.png" },
+  ];
+
 function App() {
+  const [cards, setCards] = useState([]);
+
+  function shuffleCards() {
+    //setCards(null)
+    const shuffledCards = [...initialCards, ...initialCards]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+  }
+
+console.log(cards)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className="grid">
+        {cards.map(card => (
+          <div className='card' key={card.id}>
+            <div>
+              <img className='front' src={card.src} alt="card front" />
+              <img className='back' src="/images/card_back.png" alt="card back" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
